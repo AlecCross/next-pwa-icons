956865e219ea63c26296aea2ddc0eb734683d314
AlecCross <cross2472@gmail.com>
2025-03-30 20:51:14 +0000

    MetaTags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,26 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Іконки */}
-        <link rel="icon" href="/icon-16.ico" />
-        <link rel="icon" type="image/webp" sizes="32x32" href="/icon-32.webp" />
-        <link rel="icon" type="image/webp" sizes="16x16" href="/icon-16.webp" />
-        <link rel="manifest" href="/manifest.json" />
+        <Head>
+          <link rel="icon" type="image/webp" sizes="32x32" href="/icon-32.webp"/>
+          <link rel="icon" type="image/webp" sizes="16x16" href="/icon-16.webp"/>
+          <link rel="apple-touch-icon" href="/icon-192.webp"/>
+
+          <meta property="og:type" content="website"/>
+          <meta property="og:title" content="PWA-Icons"/>
+          <meta property="og:description" content="A simple PWA for storing and converting icons"/>
+          <meta property="og:image" content="/icon-512.webp"/>
+          <meta property="og:image:type" content="image/webp"/>
+          <meta property="og:image:width" content="512"/>
+          <meta property="og:image:height" content="512"/>
+          <meta property="og:url" content="https://next-pwa-icons.vercel.app"/>
+
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:title" content="PWA-Icons"/>
+          <meta name="twitter:description" content="A simple PWA for storing and converting icons"/>
+          <meta name="twitter:image" content="/icon-512.webp"/>
+
+          <link rel="manifest" href="/manifest.json"/>
       </Head>
       <body>
         <Main />
