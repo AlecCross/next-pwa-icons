49843cb7cc1218a7218b0a9499f4a5abce7136d8
AlecCross <cross2472@gmail.com>
2025-04-30 18:31:11 +0000

    Add some styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import ConvertImage from '../components/ConvertImage';
 
 const Home = () => (
   <div>
-    <h1>Convert Image to WebP</h1>
-    <div style={{ marginBottom: '20px' }}>
+    <h1 style={{textAlign: 'center'}}>Convert Image to WebP</h1>
+    <div style={{textAlign: 'center', marginBottom: '20px' }}>
       <span>Upload an image for PWA icons:</span>
       <ConvertImage />
     </div>
